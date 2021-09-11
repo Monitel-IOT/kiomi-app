4e7388bf82a2c586cd1731010d5b44c79bf46e0a
Cristian Benites <130351@unsaac.edu.pe>
2021-09-10 19:16:16 -0500

    modificaciones a models
    .

--- kiomi_app/products/admin.py
+++ kiomi_app/products/admin.py
@@ -1,7 +1,8 @@
-from products.models import Customer, Order, OrderItem, Product, ShippingAddress
+from products.models import Customer, Order, OrderItem, Product, ShippingAddress, CategoriaProd
 from django.contrib import admin
 
 admin.site.register(Customer)
+admin.site.register(CategoriaProd)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
--- kiomi_app/products/models.py
+++ kiomi_app/products/models.py
@@ -2,79 +2,90 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+
 class Customer(models.Model):
     user = models.OneToOneField(
-			User,
-			null=True, 
-			blank=True, 
-			on_delete=models.CASCADE
-		)
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
     name = models.CharField(
-			max_length=200, 
-			null=True
-		)
+        max_length=200,
+        null=True
+    )
     email = models.EmailField(max_length=200)
 
     class Meta:
         verbose_name = 'Customer'
         verbose_name_plural = 'Customers'
         ordering = ['user']
-    
+
     def __str__(self):
         return self.name
 
+
+# Por el momento solo se han modificado el product y la categoriaProd
+class CategoriaProd(models.Model):  # categoria del producto (galleta, torta, etc.)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     price = models.FloatField()
-    digital = models.BooleanField(
-			default=False,
-			null=True,
-			blank=False
-		)
-    image = models.ImageField(null=True, blank=True)
+    description = models.CharField(max_length=1000)
+    categoria = models.ForeignKey(CategoriaProd, on_delete=models.CASCADE)
+    image_1 = models.ImageField(null=True, blank=True)
+    image_2 = models.ImageField(null=True, blank=True)
+    image_3 = models.ImageField(null=True, blank=True)
+    flavor = models.CharField(max_length=100)
 
     class Meta:
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
         ordering = ['name']
-    
+
     def __str__(self):
         return self.name
-    
-    @property
-    def imageURL(self):
-        try:
-            url = self.image.url
-        except:
-            url = ""
-        return url
+
+    # @property
+    # def imageURL(self):
+    #    try:
+    #        url = self.image.url
+    #    except:
+    #        url = ""
+    #    return url
+
 
 class Order(models.Model):
     customer = models.ForeignKey(
-			Customer, 
-			on_delete=models.SET_NULL, 
-			null=True,
-			blank=True
-		)
+        Customer,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(
-			default=False, 
-			null=True, 
-			blank=False
-		)
-    transaction_id = models.CharField(max_length=100,null=True)
+        default=False,
+        null=True,
+        blank=False
+    )
+    transaction_id = models.CharField(max_length=100, null=True)
 
     class Meta:
         verbose_name = 'order'
         verbose_name_plural = 'orders'
         ordering = ['date_ordered']
-    
+
     @property
     def get_cart_total(self):
         orderitems = self.orderitem_set.all()
         total = sum([item.get_total for item in orderitems])
         return total
-    
+
     @property
     def get_cart_items(self):
         orderitems = self.orderitem_set.all()
@@ -84,22 +95,23 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+
 class OrderItem(models.Model):
     product = models.ForeignKey(
-			Product, 
-			on_delete=models.SET_NULL,
-			null=True
-		)
+        Product,
+        on_delete=models.SET_NULL,
+        null=True
+    )
     order = models.ForeignKey(
-			Order, 
-			on_delete=models.SET_NULL, 
-			null=True
-		)
+        Order,
+        on_delete=models.SET_NULL,
+        null=True
+    )
     quantity = models.IntegerField(
-			default=0, 
-			null=True, 
-			blank=True
-		)
+        default=0,
+        null=True,
+        blank=True
+    )
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -115,23 +127,22 @@ class OrderItem(models.Model):
     def __str__(self):
         return str(self.id)
 
-    
-    
+
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(
-			Customer, 
-			on_delete=models.SET_NULL,
-			null=True
-		)
+        Customer,
+        on_delete=models.SET_NULL,
+        null=True
+    )
     order = models.ForeignKey(
-			Order, 
-			on_delete=models.SET_NULL,
-			null=True
-		)
-    address = models.CharField(max_length=200,null=False)
-    city = models.CharField(max_length=200,null=False)
-    state = models.CharField(max_length=200,null=False)
-    zipcode = models.CharField(max_length=200,null=False)
+        Order,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -140,4 +151,4 @@ class ShippingAddress(models.Model):
         ordering = ['id']
 
     def __str__(self):
-        return self.address 
+        return self.address
