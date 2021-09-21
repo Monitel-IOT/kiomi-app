4d9d7dbda787b59dac2ef71329aa933acd983d41
Cristian Benites <130351@unsaac.edu.pe>
2021-09-20 21:29:33 -0500

    funcionalidad del product-detail
    se añadio la funcionalidad para que al hacer click en una miniatura (de las tres imagenes que se tiene por producto) se ponga como la imagen principal

--- kiomi_app/static/js/components/ProductDetails.js
+++ kiomi_app/static/js/components/ProductDetails.js
@@ -1,23 +1,29 @@
-import api from '../api.js'
+import api from "../api.js";
 
 new Vue({
-  delimiters: ['[[', ']]'],
-  el: '#product-details',
+  delimiters: ["[[", "]]"],
+  el: "#product-details",
   data() {
     return {
       dbProductDetails: [],
-			message: 'hello World!!'
-    }
+      message: "hello World!!",
+      imgPrincipal: 0,
+    };
   },
   created() {
-		const search = window.location.search
-		const urlParam = new URLSearchParams(search)
-		const id = parseInt(urlParam.get('id'))
-		// console.log('id', id)
+    const search = window.location.search;
+    const urlParam = new URLSearchParams(search);
+    const id = parseInt(urlParam.get("id"));
+    // console.log('id', id)
     api
       .getProductDetails(id)
-      .then((product) => this.dbProductDetails = product)
+      .then((product) => (this.dbProductDetails = product));
   },
 
-
-})
+  methods: {
+    changeImage(num) {
+      this.imgPrincipal = num;
+      //console.log(this.imgPrincipal);
+    },
+  },
+});
