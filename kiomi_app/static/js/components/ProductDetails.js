45df17f3fc30b9260f433b1480fa459c396cf0a3
torvic <130111@unsaac.edu.pe>
2021-09-20 22:19:05 -0500

    Feat: Añadi utils para el uso de local Storage, agregue funciobalidad a product details

--- kiomi_app/static/js/components/ProductDetails.js
+++ kiomi_app/static/js/components/ProductDetails.js
@@ -1,4 +1,5 @@
 import api from '../api.js'
+import { LocalStorage } from '../utils/LocalStorage.js'
 
 new Vue({
   delimiters: ['[[', ']]'],
@@ -6,18 +7,60 @@ new Vue({
   data() {
     return {
       dbProductDetails: [],
-			message: 'hello World!!'
+			dbOrderItem: {},
+			quantity: 0,
+			flavor: ''
     }
   },
   created() {
-		const search = window.location.search
-		const urlParam = new URLSearchParams(search)
-		const id = parseInt(urlParam.get('id'))
-		// console.log('id', id)
+		/*
+			Obtener los detalles del producto de la api
+		*/
     api
-      .getProductDetails(id)
+      .getProductDetails(this.idDetails())
       .then((product) => this.dbProductDetails = product)
   },
-
+	watch: {
+		/*
+		* Observa cambios en quantiy
+		*/
+		quantity (newValue) {
+			this.addDbOrderItem('quantity', newValue)
+		},
+		/*
+		* Observa cambios en flavor
+		*/
+		flavor (newValue) {
+			this.addDbOrderItem('flavor', newValue)
+		}
+	},
+	methods: {
+		/*
+		*	Obtener el id pasado como query parmas, 
+		*	de la url de la pagina actual.
+		*/
+		idDetails () {
+			const search = window.location.search
+			const urlParam = new URLSearchParams(search)
+			return parseInt(urlParam.get('id'))
+		},
+		/*
+		*	Añade un nuevo atributo al objeto dbOrderItem,
+		*	parametros de entrada una clave y valor del atributo
+		*/
+		addDbOrderItem(key, value) {
+			this.dbOrderItem = {
+				...this.dbOrderItem, 
+				[key]: value
+			}
+		},
+		/*
+		*	Evento del button ir al carrito, añade el objeto dbOrderItem
+		* al localStorage del navegador
+		*/
+		handleGoCart() {
+			LocalStorage.saveOrderItem(this.dbOrderItem)
+		}
+	}
 
 })
