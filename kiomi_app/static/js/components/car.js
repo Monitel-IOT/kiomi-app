b2601475a31a1be5695e244e0c0db65032ec58d5
HenryCodeT <120686@unsaac.edu.pe>
2021-09-22 20:28:09 -0500

    add car html add car js

---
+++ kiomi_app/static/js/components/car.js
@@ -0,0 +1,18 @@
+import api from "../api.js";
+new Vue({
+  delimiters: ["[[", "]]"],
+  el: "#car",
+  data() {
+    return {
+      
+    }
+  },
+
+  created() {   
+    
+  },
+  methods: {
+    
+    
+  },
+});
